013bfd90a3aca336dbdbef7c5a1ee3fefabf1390
JooSut <joona.sutinen@gmail.com>
2025-09-14 11:53:17 +0300

    A1_T7 ready

--- A1_T7.py
+++
@@ -1,7 +0,0 @@
-print("Calculate fuel consumption.")
-feed = input("Enter travel distance (kilometers): ")
-Distance = int(feed)
-feed = input("Enter fuel usage (liters):")
-FuelUsage = int(feed)
-Consumption = Distance / FuelUsage
-print(f"Fuel consumption is {Consumption} l per 100 km.")
