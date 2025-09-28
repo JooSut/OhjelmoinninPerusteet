f501921f3e94120ab1c08e1b5d38b9326772db40
JooSut <joona.sutinen@gmail.com>
2025-09-29 01:41:29 +0300

    A2_T7 valmis

---
+++ W2/A2_T7.py
@@ -0,0 +1,9 @@
+print("Program starting.")
+
+Fahrenheits = float(input("Insert Fahrenheits: "))
+
+Celsius = (Fahrenheits - 32) / 1.8
+
+print(f"{round(Fahrenheits)}°F is {round(Celsius,2)}°C")
+
+print("Program is ending.")
