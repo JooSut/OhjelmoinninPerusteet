a4ba8fcb337df9e8b285aaea380cc2b02be9feaa
JooSut <joona.sutinen@gmail.com>
2025-10-01 18:06:34 +0300

    ready

---
+++ W3/A3_T3.py
@@ -0,0 +1,17 @@
+print("Program starting.")
+print("This is a program with simple menu, where you can choose which operation the program performs.")
+name = input("Before the menu, please insert your name: ")
+print("")
+print("Options:")
+print("1 - Print welcome message")
+print("0 - Exit")
+choice = int(input("Your choice: "))
+if(choice == 1):
+    print(f"Welcome {name}!")
+elif(choice == 0):
+    print("Exiting...")
+else:
+    print("Unknown option")
+
+print("")
+print("Program ending.")
