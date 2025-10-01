4ae73d21c0675c139e7786e14afc4323a913523c
JooSut <joona.sutinen@gmail.com>
2025-10-01 11:40:32 +0300

    ready

---
+++ W3/A3_T1.py
@@ -0,0 +1,24 @@
+print("Program starting.")
+print("Insert two integers.")
+Int1 = int(input("Insert firts integer: "))
+Int2 = int(input("Insert second integer: "))
+print("Comparing inserted integers.")
+
+if(Int1 > Int2):
+    print("First integer is greater.")
+elif(Int2 > Int1):
+    print("Second integer is greater.")
+else:
+    print("Integers are the same.")
+print("")
+
+print("Adding itegers together.")
+Sum = Int1 + Int2
+print(f"{Int1} + {Int2} = {Sum}")
+print("")
+Remainder = Sum % 2
+if (Remainder == 0):
+        print("Sum is even.")
+else:
+        print("Sum is odd.")
+print("Program ending.")
