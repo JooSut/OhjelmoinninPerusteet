7a7c95e3a6bde388c98574dbce13b7e655e14801
JooSut <joona.sutinen@gmail.com>
2025-10-26 10:47:13 +0200

    ready

---
+++ W4/A4_T1.py
@@ -0,0 +1,14 @@
+print("Program starting")
+print("")
+
+start = int(input("Insert starting value: "))
+stop = int(input("Insert stopping value: "))
+#Use these integers together with the “for-loop” structure to create behaviour like in the example program example run below.
+
+print("")
+print("Starting for-loop:")
+for i in range(start, stop +1):
+    print(i)
+#Note! the autograding tool will test that the correct structure has been applied.
+
+print("\nProgram ending.")
