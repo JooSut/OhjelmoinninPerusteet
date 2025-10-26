2114817f41b46db89e1280d51daa51282881497f
JooSut <joona.sutinen@gmail.com>
2025-10-26 13:15:46 +0200

    ready

---
+++ W4/A4_T5.py
@@ -0,0 +1,38 @@
+print("Program starting.")
+print("")
+feed = input("Insert starting point: ")
+PointStart = int(feed)
+feed = input("insert stopping point: ")
+PointStop = int(feed)
+feed = input("Insert inspection point: ")
+PointInspect = int(feed)
+print("")
+
+Run = True
+
+if(PointStart >= PointStop):
+    print("Starting point value must be less than the stopping point value.")
+if((PointStart > PointInspect) or (PointInspect > PointStop)):
+    print("Inspection value must be within the range of start and stop.")
+    Run = False
+
+if(Run):
+    print("First loop - inspection with break:")
+    for i in range(PointStart, PointStop):
+        if(i == PointInspect):
+            break
+        else:
+            print(i, end=' ')
+    print("")
+    print("Second loop - inspection with continue:")
+    for i in range(PointStart, PointStop):
+        if(i == PointInspect):
+            continue
+        else:
+            if(i == (PointStop-1)):
+                print(i)
+            else:
+                print(i, end=' ')
+            
+
+    print("\nProgram ending.")
