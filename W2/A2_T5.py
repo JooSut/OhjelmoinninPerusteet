44375e6c45664ddfb2e564f625286f2b180de685
JooSut <joona.sutinen@gmail.com>
2025-10-31 18:14:37 +0200

    ready

--- W2/A2_T5.py
+++ W2/A2_T5.py
@@ -11,14 +11,12 @@ print(f"The inserted word length is {length}")
 #Last character:
 lc = word[-1]
 print(f"Last character is '{lc}'\n")
-
 #subtstring:
 print("Take substring from the inserted word by inserting...")
 start = int(input("1) Starting point: "))
 end = int(input("2) Ending point: "))
 step = int(input("3) Step size: "))
 substring = word[start:end:step]
-
 print(f"\nThe word '{word}' sliced to the defined substring is '{substring}'.")
 #End program:
 print("Program ending.")
